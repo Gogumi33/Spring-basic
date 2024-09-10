13cef5feb6d143b7d86d36c153d238752e4f7428
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-09-10 17:02:54 +0900

    adapt java 21

--- src/main/java/hi/hi_spring/HiSpringApplication.java
+++ src/main/java/hi/hi_spring/HiSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HiSpringApplication {
-
+	// adapt java 21
 	public static void main(String[] args) {
 		SpringApplication.run(HiSpringApplication.class, args);
 	}
